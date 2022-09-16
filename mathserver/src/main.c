88cf4caa0e3e797dbe70357be220cad7002ad265
David Värmfors <david.varmfors@gmail.com>
2022-09-16 14:39:39 +0200

    Requirements + mek session
    Listing of all requirements in the readme

---
+++ mathserver/src/main.c
@@ -0,0 +1,9 @@
+// Main file for mathserver
+// @authors David Värmfors & Albin Södervall
+#include <stdio.h>
+
+// Main function, running the server
+int main(int argc, char *argv[]) {
+    printf("Köp mer SBB grabbar, TOO THE MOON!");
+    return 0;
+}
